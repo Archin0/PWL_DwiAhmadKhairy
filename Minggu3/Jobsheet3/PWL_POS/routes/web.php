95ce1f1740dad771b4708379eb5e5b7a6cfbf338
Dwi Ahmad Khairy <121588135+Archin0@users.noreply.github.com>
2025-03-05 09:28:39 +0700

    praktikum: Implementasi Query Builder

--- Minggu3/Jobsheet3/PWL_POS/routes/web.php
+++ Minggu3/Jobsheet3/PWL_POS/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
@@ -24,15 +25,16 @@ use App\Http\Controllers\SalesController;
 
 Route::get('/', HomeController::class);
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
-});
+// // Route::get('/products', [ProductController::class, 'index']);
+// // Route::prefix('category')->group(function () {
+// //     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+// //     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+// //     Route::get('/home-care', [ProductController::class, 'homeCare']);
+// //     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+// // });
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
-Route::get('/sales', [SalesController::class, 'index']);
+// Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+// Route::get('/sales', [SalesController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
