8298fcd18007378e5b1128c35231f2f1f62cfea0
Dwi Ahmad Khairy <121588135+Archin0@users.noreply.github.com>
2025-03-19 07:12:08 +0700

    Tugas Praktikum 1: Menambahkan button Add di halaman `Manage Kategori`

--- Minggu5/Jobsheet5/PWL_POS/resources/views/kategori/index.blade.php
+++ Minggu5/Jobsheet5/PWL_POS/resources/views/kategori/index.blade.php
@@ -4,12 +4,13 @@
 
 @section('subtitle', 'Kategori')
 @section('content_header_title', 'Home')
-@section('content_header_subtitle', 'Kaggeori')
+@section('content_header_subtitle', 'Kategori')
 
 @section('content')
 <div class="container">
     <div class="card">
         <div class="card-header">Manage Kategori</div>
+        <a href="{{ url('/kategori/create') }}" class="btn btn-primary mr-auto ml-3 mt-3">Tambah Kategori</a>
         <div class="card-body">
             {{ $dataTable->table() }}
         </div>
@@ -20,4 +21,3 @@
 @push('scripts')
     {{ $dataTable->scripts() }}
 @endpush
-
