dbf3ab186523a062ef45c3817ed7cca5c59678f7
Dwi Ahmad Khairy <121588135+Archin0@users.noreply.github.com>
2025-03-25 10:43:02 +0700

    Praktikum: Penerapan Layouting

--- Minggu5/Jobsheet5_revisi/PWL_POS/routes/web.php
+++ Minggu5/Jobsheet5_revisi/PWL_POS/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,10 +38,14 @@ Route::get('/', HomeController::class);
 // Route::get('/sales', [SalesController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
+
 Route::get('/kategori', [KategoriController::class, 'index']);
+
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/', [WelcomeController::class, 'index']);
