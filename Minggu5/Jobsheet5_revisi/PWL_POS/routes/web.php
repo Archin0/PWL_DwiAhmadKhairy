8eebdcf42540b24c8638f38e7a0537eb44b8a8d3
Dwi Ahmad Khairy <121588135+Archin0@users.noreply.github.com>
2025-03-26 09:53:08 +0700

    Praktikum: Implementasi jQuery Datatable di AdminLTE

--- Minggu5/Jobsheet5_revisi/PWL_POS/routes/web.php
+++ Minggu5/Jobsheet5_revisi/PWL_POS/routes/web.php
@@ -24,7 +24,7 @@ use App\Http\Controllers\WelcomeController;
 //     return view('welcome');
 // });
 
-Route::get('/', HomeController::class);
+// Route::get('/', HomeController::class);
 
 // // Route::get('/products', [ProductController::class, 'index']);
 // // Route::prefix('category')->group(function () {
@@ -37,15 +37,26 @@ Route::get('/', HomeController::class);
 // Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 // Route::get('/sales', [SalesController::class, 'index']);
 
-Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/level', [LevelController::class, 'index']);
 
-Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
 
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);          //menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);   //menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);         //menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);       //menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);  //menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);     //menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
+});
