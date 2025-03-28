cfaab38ce3cb612657385d3db7cbf79c44ba03b9
Dwi Ahmad Khairy <121588135+Archin0@users.noreply.github.com>
2025-03-28 16:53:49 +0700

    Tugas: Implementasi Ajax Form dan Client Validation dengan jQuery Validation pada tabel m_kategori

--- Minggu6/Jobsheet6/PWL_POS/routes/web.php
+++ Minggu6/Jobsheet6/PWL_POS/routes/web.php
@@ -102,9 +102,19 @@ Route::group(['prefix' => 'kategori'], function () {
     Route::post('/list', [KategoriController::class, 'list']);      // menampilkan data kategori dalam bentuk json untuk datatables
     Route::get('/create', [KategoriController::class, 'create']);   // menampilkan halaman form tambah kategori
     Route::post('/', [KategoriController::class, 'store']);         // menyimpan data kategori baru
+
+    Route::get('/create_ajax', [KategoriController::class, 'create_ajax']);
+    Route::post('/ajax', [KategoriController::class, 'store_ajax']);
+    
     Route::get('/{id}', [KategoriController::class, 'show']);       // menampilkan detail kategori
     Route::get('/{id}/edit', [KategoriController::class, 'edit']);  // menampilkan halaman form edit kategori
     Route::put("/{id}", [KategoriController::class, 'update']);     // menyimpan perubahan data kategori
+
+    Route::get('/{id}/edit_ajax', [KategoriController::class, 'edit_ajax']);
+    Route::put('/{id}/update_ajax', [KategoriController::class, 'update_ajax']);
+    Route::get('/{id}/delete_ajax', [KategoriController::class, 'confirm_ajax']);
+    Route::delete('/{id}/delete_ajax', [KategoriController::class, 'delete_ajax']);
+    
     Route::delete('/{id}', [KategoriController::class, 'destroy']); // menghapus data kategori
 });
 
